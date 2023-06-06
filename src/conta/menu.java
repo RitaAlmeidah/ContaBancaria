73b89357d9699764064a8070ce058409b7f3bb99
rita_@Almeida <rita_aja92@hotmail.com>
2023-06-06 11:59:58 -0300

    Atividade.06.06.2023

--- src/conta/menu.java
+++ src/conta/menu.java
@@ -10,14 +10,6 @@ public class menu {
 
 	public static void main(String[] args) 
 	{
-		
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
-		
 		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
 		cc1.visualizar();
 		cc1.sacar(12000.0f);
